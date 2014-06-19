7af4fb937fcb5b7cc0fd0655eef5206d1fac1975
dhmn <dhmn@dhmnprinter.lan>
2014-06-19 16:31:29 -0500

    had lots of overshoot. re-ran autotune

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -160,9 +160,9 @@
 // When temperature exceeds max temp, your heater will be switched off.
 // This feature exists to protect your hotend from overheating accidentally, but *NOT* from thermistor short/failure!
 // You should use MINTEMP for thermistor short/failure protection.
-#define HEATER_0_MAXTEMP 275
-#define HEATER_1_MAXTEMP 275
-#define HEATER_2_MAXTEMP 275
+#define HEATER_0_MAXTEMP 250
+#define HEATER_1_MAXTEMP 250
+#define HEATER_2_MAXTEMP 250
 #define BED_MAXTEMP 150
 
 // If your bed has low resistance e.g. .6 ohm and throws the fuse you can duty cycle it to reduce the
@@ -178,17 +178,17 @@
 #ifdef PIDTEMP
   //#define PID_DEBUG // Sends debug data to the serial port.
   //#define PID_OPENLOOP 1 // Puts PID in open loop. M104/M140 sets the output power from 0 to PID_MAX
-  #define PID_FUNCTIONAL_RANGE 10 // If the temperature difference between the target temperature and the actual temperature
+  #define PID_FUNCTIONAL_RANGE 20 // If the temperature difference between the target temperature and the actual temperature
                                   // is more then PID_FUNCTIONAL_RANGE then the PID will be shut off and the heater will be set to min/max.
   #define PID_INTEGRAL_DRIVE_MAX 255  //limit for the integral term
   #define K1 0.95 //smoothing factor within the PID
   #define PID_dT ((16.0 * 8.0)/(F_CPU / 64.0 / 256.0)) //sampling period of the temperature routine
 
 // If you are using a preconfigured hotend then you can use one of the value sets by uncommenting it
-// autotuned arount 185c
-    #define  DEFAULT_Kp 25.36
-    #define  DEFAULT_Ki 2.13  
-    #define  DEFAULT_Kd 75.63  
+// autotuned arount 210c
+    #define  DEFAULT_Kp 23
+    #define  DEFAULT_Ki 2  
+    #define  DEFAULT_Kd 70  
 
 // Ultimaker
 //    #define  DEFAULT_Kp 22.2
